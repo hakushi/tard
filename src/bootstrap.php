b85b3930d28fe09d0cd8e3ccba78d1688d723320
David Edström <hakushi242@gmail.com>
2014-09-17 20:53:49 +0200

    added dump() and getCurrentURL() functions

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -18,3 +18,18 @@ function myAutoloader($class) {
   }
 }
 spl_autoload_register('myAutoloader');
+
+function dump($array) {
+  echo "<pre>" . htmlentities(print_r($array, 1)) . "</pre>";
+}
+
+function getCurrentUrl() {
+  $url = "http";
+  $url .= (@$_SERVER["HTTPS"] == "on") ? 's' : '';
+  $url .= "://";
+  $serverPort = ($_SERVER["SERVER_PORT"] == "80") ? '' :
+    (($_SERVER["SERVER_PORT"] == 443 && @$_SERVER["HTTPS"] == "on") ? '' : ":{$_SERVER['SERVER_PORT']}");
+  $url .= $_SERVER["SERVER_NAME"] . $serverPort . htmlspecialchars($_SERVER["REQUEST_URI"]);
+  return $url;
+}
+ 
